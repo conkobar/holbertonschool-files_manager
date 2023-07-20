f84d9e6a1d3899aa68cd591e821df963b9405f27
conkobar <connorkleine@gmail.com>
2023-07-20 11:11:05 -0500

    task 0 promisify approach

--- utils/redis.js
+++ utils/redis.js
@@ -1,10 +1,14 @@
 // skeleton for redis client
 const redis = require('redis');
+const { promisify } = require('util');
 
 // define redis client for export
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
     this.client.on('error', function (err) {
       console.log(err);
     });
@@ -17,34 +21,20 @@ class RedisClient {
 
   // get method for redis client
   async get(key) {
-    return new Promise((resolve, reject) => {
-      this.client.get(key, (err, reply) => {
-        if (err) return reject(err);
-        resolve(reply);
-      });
-    });
+    return await this.getAsync(key);
   }
 
   // set method for redis client
   async set(key, value, duration) {
-    return new Promise((resolve, reject) => {
-      this.client.set(key, value, 'EX', duration, (err, reply) => {
-        if (err) return reject(err);
-        resolve(reply);
-      });
-    });
+    return await this.setAsync(key, value, 'EX', duration);
   }
 
   // delete method for redis client
   async del(key) {
-    return new Promise((resolve, reject) => {
-      this.client.del(key, (err, reply) => {
-        if (err) return reject(err);
-        resolve(reply);
-      });
-    });
+    return await this.delAsync(key);
   }
 }
 
+// export our class as a new redis instance
 const redisClient = new RedisClient();
 module.exports = redisClient;
