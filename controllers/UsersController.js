ee2cec45da64c9acb8bdcb7917936d91558f0a84
conkobar <connorkleine@gmail.com>
2023-07-25 14:07:36 -0500

    task 4; should be good

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,8 @@
 // user controller for express api
 const sha1 = require('sha1');
+const mongo = require('mongodb');
 const dbClient = require('../utils/db');
+const Redis = require('../utils/redis');
 
 class UsersController {
   static postNew(req, res) {
@@ -20,6 +22,23 @@ class UsersController {
       });
       // send status code 201: created
       return res.status(201).send({ id: newUser.insertedId, email });
+    })();
+  }
+
+  // gets a user's information
+  static getMe(req, res) {
+    (async () => {
+      const token = req.headers['x-token'];
+      const user = await Redis.get(`auth_${token}`);
+      const userID = new mongo.ObjectID(user);
+      console.log(`token: ${token}, user: ${user}, userID: ${userID}`);
+      // try to fetch user data
+      const userData = await dbClient.db
+        .collection('users')
+        .findOne({ _id: userID });
+      if (!userData) return res.status(401).send({ error: 'Unauthorized' });
+      // send status code 200: OK
+      return res.status(200).send({ id: userData._id, email: userData.email });
     })();
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,16 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
 
 // declare our router
 const router = express.Router();
 // get routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 // post routes
 router.post('/users', UsersController.postNew);
 
