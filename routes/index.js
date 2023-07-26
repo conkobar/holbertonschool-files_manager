159fecd08516450def4c812f3fadd0b13e18dbfe
virtualblakewillis <bewillis101@gmail.com>
2023-07-26 12:23:54 -0500

    task 6 routes

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,8 @@ router.get('/stats', AppController.getStats);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
 // post routes
 router.post('/users', UsersController.postNew);
